3feaf7c7e7a22606baf4c163428666a2f05e5d16
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-09-24 10:56:10 +0200

    Added a controller for the pod

--- casepropods/family_connect_subscription/static/init.js
+++ casepropods/family_connect_subscription/static/init.js
@@ -1,3 +1,15 @@
-angular.module('cases', []);
 angular.module('cases.directives', []);
-angular.module('cases.controllers', []);
+angular.module('cases.controllers', [])
+  .controller('PodController', ['$q', '$scope', function($q, $scope) {
+    $scope.init = function (podConfig) {
+      $scope.podConfig = podConfig
+      $scope.status = 'loading'
+
+      $scope.update()
+        .then(function(){$scope.status = 'idle'})
+    }
+  }]);
+angular.module('cases', [
+  'cases.directives',
+  'cases.controllers'
+]);
