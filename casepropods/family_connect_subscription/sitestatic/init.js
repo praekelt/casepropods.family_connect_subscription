24c15986e0fbd91005aa0cf7c7e96833cfba92de
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-09-26 18:43:07 +0200

    Remove controller for tests

--- casepropods/family_connect_subscription/sitestatic/init.js
+++ casepropods/family_connect_subscription/sitestatic/init.js
@@ -1,14 +1,5 @@
 angular.module('cases.directives', []);
-angular.module('cases.controllers', [])
-  .controller('PodController', ['$q', '$scope', function($q, $scope) {
-    $scope.init = function (podConfig) {
-      $scope.podConfig = podConfig
-      $scope.status = 'loading'
-
-      $scope.update()
-        .then(function(){$scope.status = 'idle'})
-    }
-  }]);
+angular.module('cases.controllers', []);
 angular.module('cases', [
   'cases.directives',
   'cases.controllers'
