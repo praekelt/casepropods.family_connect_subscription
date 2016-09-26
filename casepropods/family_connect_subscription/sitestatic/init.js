d0d30426a757dba712222d53b8959c56ca64b89b
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-09-26 18:44:27 +0200

    Make tests accept using '[[ ]]' for template variables

--- casepropods/family_connect_subscription/sitestatic/init.js
+++ casepropods/family_connect_subscription/sitestatic/init.js
@@ -3,4 +3,9 @@ angular.module('cases.controllers', []);
 angular.module('cases', [
   'cases.directives',
   'cases.controllers'
-]);
+])
+.config(['$interpolateProvider', function ($interpolateProvider) {
+  // Since Django uses {{ }}, we will have angular use [[ ]] instead.
+  $interpolateProvider.startSymbol("[[");
+  $interpolateProvider.endSymbol("]]");
+}]);
